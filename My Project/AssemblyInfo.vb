0afb164da159d18564dcef2f6e43cea3c2bc6f0d
xeon <xeon@30945a82-e562-11dd-bce9-bf72c0239d3a>
2006-01-24 21:08:50 +0000

    Добавлен дополнительный проект для привязки версии сборки к версии репозитория.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -34,5 +34,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.0.2005")> 
-<Assembly: AssemblyFileVersion("2.0.0.2005")> 
+<Assembly: AssemblyVersion("2.0.2005.17")> 
+<Assembly: AssemblyFileVersion("2.0.2005.17")> 
