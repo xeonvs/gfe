870502cfd7a4a402dc88844aba06dc2594f6d81a
Xeon <Xeon@30945a82-e562-11dd-bce9-bf72c0239d3a>
2007-03-28 18:29:29 +0000

    Поддержка MS SQL Server

---
+++ mssqlBase/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("mssqlBase")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Visual Studio BBS")> 
+<Assembly: AssemblyProduct("mssqlBase")> 
+<Assembly: AssemblyCopyright("Copyright © Visual Studio BBS 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("48E96732-FE3C-4b03-93EA-6FCFF5C6957B")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.0.2007.38")> 
+<Assembly: AssemblyFileVersion("2.0.2007.38")> 
