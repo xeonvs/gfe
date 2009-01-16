20a312bec048d1c825ed1d1d00ef2ad541bfa478
xeon <xeon@30945a82-e562-11dd-bce9-bf72c0239d3a>
2009-01-16 12:42:59 +0000

    Добавлены тестовые настройки, управляющие подключением к месадж-базе.

--- frmOptions.Designer.vb
+++ frmOptions.Designer.vb
@@ -22,8 +22,104 @@ Partial Class frmOptions
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtBasePath = New System.Windows.Forms.TextBox
+        Me.cmdOk = New System.Windows.Forms.Button
+        Me.cmdCancel = New System.Windows.Forms.Button
+        Me.cmdOpen = New System.Windows.Forms.Button
+        Me.grpTossers = New System.Windows.Forms.GroupBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 172)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Tosser Config"
+        '
+        'txtBasePath
+        '
+        Me.txtBasePath.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtBasePath.Location = New System.Drawing.Point(15, 188)
+        Me.txtBasePath.Name = "txtBasePath"
+        Me.txtBasePath.Size = New System.Drawing.Size(267, 20)
+        Me.txtBasePath.TabIndex = 1
+        '
+        'cmdOk
+        '
+        Me.cmdOk.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdOk.Location = New System.Drawing.Point(15, 218)
+        Me.cmdOk.Name = "cmdOk"
+        Me.cmdOk.Size = New System.Drawing.Size(75, 23)
+        Me.cmdOk.TabIndex = 2
+        Me.cmdOk.Text = "Ok"
+        Me.cmdOk.UseVisualStyleBackColor = True
+        '
+        'cmdCancel
+        '
+        Me.cmdCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdCancel.Location = New System.Drawing.Point(258, 218)
+        Me.cmdCancel.Name = "cmdCancel"
+        Me.cmdCancel.Size = New System.Drawing.Size(75, 23)
+        Me.cmdCancel.TabIndex = 3
+        Me.cmdCancel.Text = "Cancel"
+        Me.cmdCancel.UseVisualStyleBackColor = True
+        '
+        'cmdOpen
+        '
+        Me.cmdOpen.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdOpen.Location = New System.Drawing.Point(288, 188)
+        Me.cmdOpen.Name = "cmdOpen"
+        Me.cmdOpen.Size = New System.Drawing.Size(45, 20)
+        Me.cmdOpen.TabIndex = 4
+        Me.cmdOpen.Text = "Open"
+        Me.cmdOpen.UseVisualStyleBackColor = True
+        '
+        'grpTossers
+        '
+        Me.grpTossers.Location = New System.Drawing.Point(15, 12)
+        Me.grpTossers.Name = "grpTossers"
+        Me.grpTossers.Size = New System.Drawing.Size(180, 146)
+        Me.grpTossers.TabIndex = 5
+        Me.grpTossers.TabStop = False
+        Me.grpTossers.Text = "Tossers"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(230, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 52)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Тестовые " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "настройки, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "для проверки " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "функционала."
+        '
+        'frmOptions
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "frmOptions"
+        Me.ClientSize = New System.Drawing.Size(337, 250)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.grpTossers)
+        Me.Controls.Add(Me.cmdOpen)
+        Me.Controls.Add(Me.cmdCancel)
+        Me.Controls.Add(Me.cmdOk)
+        Me.Controls.Add(Me.txtBasePath)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmOptions"
+        Me.Text = "Настройки"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtBasePath As System.Windows.Forms.TextBox
+    Friend WithEvents cmdOk As System.Windows.Forms.Button
+    Friend WithEvents cmdCancel As System.Windows.Forms.Button
+    Friend WithEvents cmdOpen As System.Windows.Forms.Button
+    Friend WithEvents grpTossers As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
