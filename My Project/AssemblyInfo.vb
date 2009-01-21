a55ae042b2726fe8c1545959c9ec4ba8b20edfb3
xeon <xeon@30945a82-e562-11dd-bce9-bf72c0239d3a>
2009-01-21 12:04:27 +0000

    build 68

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -34,5 +34,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.2005.66")> 
-<Assembly: AssemblyFileVersion("2.0.2005.66")> 
+<Assembly: AssemblyVersion("2.0.2005.68")> 
+<Assembly: AssemblyFileVersion("2.0.2005.68")> 
--- mssqlBase/My Project/AssemblyInfo.vb
+++ mssqlBase/My Project/AssemblyInfo.vb
@@ -33,5 +33,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.2007.66")> 
-<Assembly: AssemblyFileVersion("2.0.2007.66")> 
+<Assembly: AssemblyVersion("2.0.2007.68")> 
+<Assembly: AssemblyFileVersion("2.0.2007.68")> 
